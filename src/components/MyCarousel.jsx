f62e58ea60b06b5586c42e80b96e2c14c75f5cf8
rmccrear <113715288+rmccrear@users.noreply.github.com>
2023-08-27 10:08:24 +0000

    carousel image

--- src/components/MyCarousel.jsx
+++ src/components/MyCarousel.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import Carousel from 'react-bootstrap/Carousel';
 
-
 const slushyImgSrc = "../assets/francesca-hotchin-DSxurmhrfuc-unsplash.jpg";
 
 class MyCarousel extends Component {
@@ -11,18 +10,7 @@ class MyCarousel extends Component {
   render() {
     return ( 
       <Carousel>
-        <Carousel.Item>
-          <img src={slushyImgSrc} />
-          <Carousel.Caption>
-            <h3>First slide label</h3>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img src={slushyImgSrc} />
-          <Carousel.Caption>
-            <h3>Second slide label</h3>
-          </Carousel.Caption>
-        </Carousel.Item>
+
       </Carousel>
     );
   }
